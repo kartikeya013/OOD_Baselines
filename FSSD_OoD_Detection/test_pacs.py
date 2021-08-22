622202ae23b20cbe50b864bd902472bbfdec4bc4
Kartikeya Gupta <kartikeyagupta2000@gmail.com>
2021-08-22 21:24:19 +0530

    adding imagenet-r

--- FSSD_OoD_Detection/test_pacs.py
+++ FSSD_OoD_Detection/test_pacs.py
@@ -1,10 +1,12 @@
 import subprocess
 
 dist = ['photo','art_painting','cartoon','sketch']
+method = ['baseline','odin','maha']
 
 
-for ind in dist:
-    for ood in dist:
-        f = open('./odin/'+ind+'_'+ood+".txt", "w")
-        subprocess.call(['python', 'test_odin_pacs.py', '--ind',ind,'--ood',ood,'--model_arch','resnet'],stdout=f)
-        print(ind+'_'+ood+'Done')
+for m in method:
+	for ind in dist:
+    		for ood in dist:
+        		f = open('./'+m+'/'+ind+'_'+ood+".txt", "w")
+        		subprocess.call(['python', 'test_'+m+'_pacs.py', '--ind',ind,'--ood',ood,'--model_arch','resnet'],stdout=f)
+        		print('############################\n'+'Method: '+m+'\n\t--> In-distribution:'+ind+'\n\t--> Out-Distribution:'+ood+'\nDone')
